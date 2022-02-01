f360d5ff381c6d36238b8732a39b9a2858f1cb5f
"Norapat Buppodom" <"new17353@gmail.com">
2022-02-01 11:44:18 +0700

    update code

--- Hello.sol
+++ Hello.sol
@@ -4,13 +4,32 @@ pragma solidity ^0.8.0;
 
 contract Hello {
     string public message = "Hello wrold";
-    // uint count;
+    uint count;
+    bool b = true;
+    mapping(address => uint) balance;
+    struct MyStruct {
+        string a;
+        uint b;
+    }
+    uint constant w = 5;
+    uint immutable x1;
 
-    // function inc() {
-    //     count++;
-    // }
+    constructor() {
+        x1 = 5;
+        balance[0x5B38Da6a701c568545dCfcB03FcB875f56beddC4] = 100;
+    }
 
-    // function dec() {
-    //     count--;
-    // }
+    function inc() public {
+        count++;
+    }
+
+    function getCount() public view returns (uint) {
+        return count;
+    }
+
+    function foo() public view returns (uint a, uint ) {
+        a = 5;
+        return (a, balance[0x5B38Da6a701c568545dCfcB03FcB875f56beddC4]);
+    }
+
 }
