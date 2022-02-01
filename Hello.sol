6b7f531e654f3c2a3c39647da6f1fb70edde7766
"Norapat Buppodom" <"new17353@gmail.com">
2022-02-01 11:25:46 +0700

    first commit

---
+++ Hello.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Hello {
+    string public message = "Hello wrold";
+    // uint count;
+
+    // function inc() {
+    //     count++;
+    // }
+
+    // function dec() {
+    //     count--;
+    // }
+}
